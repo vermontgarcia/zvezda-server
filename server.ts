dca12dd0e8a973c54821cdc31613b03e723a54da
Vermont Garcia <vermontgarcia@gmail.com>
2025-05-11 00:17:42 -0600

    Configuration update

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
-import app from './src/app.ts';
+import app from './src/app.js';
 // import { initDB } from './src/database/db';
-import { PORT } from './src/utils/const.env.ts';
+import { PORT } from './src/utils/const.env.js';
 
 const port = PORT || 8080;
 
